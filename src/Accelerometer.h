cb427756aa6bb2f1e306c73fcd2205a3698dd109
Lena Voytek <lena@ieee.org>
2020-05-28 15:31:05 -0700

    Added accelerometer function headers

--- src/Accelerometer.h
+++ src/Accelerometer.h
@@ -41,9 +41,94 @@ protected:
 	BaseAccelerometer * accel;
 	SupportedSensor sensorType;
 
-public:
+	//Callibration offsets for angles
+	float pitchOffset;
+	float rollOffset;
+	float yawOffset;
 
+	//Callibration offsets for acceleration
+	float upwardAccelOffset;
+	float forwardAccelOffset;
+	float lrAccelOffset;
+
+	//Most recent angle measurements
+	float currentPitch;
+	float currentRoll;
+	float currentYaw;
+
+	//Most recent acceleration measurements
+	float currentUp;
+	float currentLeft;
+	float currentForward;
+
+public:
+	/**
+	 * @brief Initialize the sensor object matching the given type
+	 *
+	 * @param sensorType The enum for the type of sensor being used
+	 */
 	Accelerometer(SupportedSensor sensorType);
+
+	/**
+	 * @brief Attempt to initialize communication with the given accelerometer
+	 *
+	 * @return
+	 * 		- true accelerometer activated
+	 * 		- accelerometer unavailable
+	 */
+	bool init();
+
+	/**
+	 * @brief Callibrate the accelerometer by getting current values assuming the drone is on a perfectly flat surface and not moving
+	 */
+	void callibrate();
+
+	/**
+	 * @brief Read all angles and accelerations and store locally
+	 */
+	void update();
+
+	/**
+	 * @brief Get the current pitch angle in degrees
+	 *
+	 * @return The current pitch in degrees
+	 */
+	float getPitch();
+
+	/**
+	 * @brief Get the current roll angle in degrees
+	 *
+	 * @return The current roll angle in degrees
+	 */
+	float getRoll();
+
+	/**
+	 * @brief Get the current yaw offset from startup in degrees
+	 *
+	 * @return The current yaw angle in degrees
+	 */
+	float getYaw();
+
+	/**
+	 * @brief Get the current forward acceleration (Gs)
+	 *
+	 * @return The current forward acceleration as compared to 1G
+	 */
+	float getAccelForward();
+
+	/**
+	 * @brief Get the current left-right acceleration (Gs)
+	 *
+	 * @return The current left-right acceleration as compared to 1G (left is positive)
+	 */
+	float getAccelLR();
+
+	/**
+	 * @brief Get the current upward acceleration (Gs)
+	 *
+	 * @return The current upward acceleration as compared to 1G
+	 */
+	float getAccelZ();
 };
 
 #endif
