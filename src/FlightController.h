da64669e3cf1e29d3083e16750e562ab9d024785
Lena Voytek <dvoytek@email.arizona.edu>
2020-06-02 12:15:11 -0700

    Added flight control command headers

--- src/FlightController.h
+++ src/FlightController.h
@@ -73,6 +73,92 @@ public:
 	 * 		- false some motors still armed
 	 */
 	bool kill();
+
+	/**
+	 * @brief Throttle all motors on the aircraft to a certain percentage
+	 * 
+	 * @param speed The throttle percentage of the motors
+	 * 
+	 * @return
+	 * 		- true Speed change success
+	 * 		- false Speed change failure
+	 */
+	bool throttleAll(float speed);
+
+	/**
+	 * @brief Make a series of throttle changes to reorient the aircraft to have no tilt and level out altitude
+	 * 
+	 * @return
+	 * 		- true Drone managed to reorient itself automatically
+	 * 		- false reorientation failed
+	 */
+	bool reorient();
+
+	/**
+	 * @brief Tilt the aircraft and begin moving forward while maintaining altitude
+	 * 
+	 * @param speed The forward speed percentage corresponding to tilt angle and motor speed
+	 * 
+	 * @return
+	 * 		- true tilt and speed adjust success
+	 * 		- false tilt unsucessful or undetected
+	 */
+	bool forward(float speed);
+
+	/**
+	 * @brief Tilt the aircraft and begin moving backward while maintaining altitude
+	 * 
+	 * @param speed The reverse speed percentage corresponding to tilt angle and motor speed
+	 * 
+	 * @return
+	 * 		- true tilt and speed adjust success
+	 * 		- false tilt unsucessful or undetected
+	 */
+	bool reverse(float speed);
+
+	/**
+	 * @brief Tilt the aircraft and begin moving left while maintaining altitude
+	 * 
+	 * @param speed The leftward speed percentage corresponding to tilt angle and motor speed
+	 * 
+	 * @return
+	 * 		- true tilt and speed adjust success
+	 * 		- false tilt unsucessful or undetected
+	 */
+	bool left(float speed);
+
+	/**
+	 * @brief Tilt the aircraft and begin moving right while maintaining altitude
+	 * 
+	 * @param speed The rightward speed percentage corresponding to tilt angle and motor speed
+	 * 
+	 * @return
+	 * 		- true tilt and speed adjust success
+	 * 		- false tilt unsucessful or undetected
+	 */
+	bool right(float speed);
+
+	/**
+	 * @brief Yaw the aircraft clockwise by increasing cw-oriented motor throttle and decreasing ccw-oriented motor throttle
+	 * 
+	 * @param speed The clockwise speed corresponding to the variation in throttle between cw and ccw motors
+	 * 
+	 * @return
+	 * 		- true motor speeds changed
+	 * 		- false rotation initialization unsuccessful
+	 */
+	bool yawCW(float speed);
+
+	/**
+	 * @brief Yaw the aircraft counter-clockwise by increasing ccw-oriented motor throttle and decreasing cw-oriented motor throttle
+	 * 
+	 * @param speed The counter-clockwise speed corresponding to the variation in throttle between ccw and cw motors
+	 * 
+	 * @return
+	 * 		- true motor speeds changed
+	 * 		- false rotation initialization unsuccessful
+	 */
+	bool yawCCW(float speed);
 };
 
 #endif
